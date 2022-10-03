b754e9beb221bf1b09cd71eea3324cfca6e73010
Lisandro Marchionni <djlm2140@gmail.com>
2022-10-03 13:37:57 -0300

    Added .env support for Render deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Match } from './matchs/entities/matchs.entity';
 import { Team } from './teams/entities/team.entity';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 @Module({
   imports: [
     TeamsModule,
     MatchsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'fvl',
-      password: 'segura123!',
-      database: 'db_fvl',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Match, Team],
       synchronize: true,
     }),
