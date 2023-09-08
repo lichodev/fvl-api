e84323c147cc92d01a69fafe4a311696243f516e
Lisandro Marchionni <marchionni.lisandro@gmail.com>
2023-09-07 23:33:10 -0300

    test: added testing for controllers

--- src/matchs/matchs.controller.spec.ts
+++ src/matchs/matchs.controller.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MatchsController } from './matchs.controller';
+import { MatchsService } from './matchs.service';
 
 describe('MatchsController', () => {
   let controller: MatchsController;
 
+  const mockMatchsService = {
+    add: jest.fn((dto) => {
+      return {
+        ...dto,
+        id: Date.now(),
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MatchsController],
-    }).compile();
+      providers: [MatchsService],
+    })
+      .overrideProvider(MatchsService)
+      .useValue(mockMatchsService)
+      .compile();
 
     controller = module.get<MatchsController>(MatchsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a team and return it with its id', () => {
+    expect(
+      controller.addNewMatch({
+        local: 1,
+        visitante: 2,
+        sets_local: 2,
+        sets_visitante: 3,
+      }),
+    ).toEqual({
+      id: expect.any(Number),
+      local: 1,
+      visitante: 2,
+      sets_local: 2,
+      sets_visitante: 3,
+    });
   });
 });
--- src/teams/teams.controller.spec.ts
+++ src/teams/teams.controller.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TeamsController } from './teams.controller';
+import { TeamsService } from './teams.service';
 
 describe('TeamsController', () => {
   let controller: TeamsController;
 
+  const mockTeamsService = {
+    add: jest.fn((dto) => {
+      return {
+        id: Date.now(),
+        ...dto,
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TeamsController],
-    }).compile();
+      providers: [TeamsService],
+    })
+      .overrideProvider(TeamsService)
+      .useValue(mockTeamsService)
+      .compile();
 
     controller = module.get<TeamsController>(TeamsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a team and return it with its id', () => {
+    expect(
+      controller.addNewTeam({
+        country: 'AR',
+        location: 'Miramar',
+        name: 'Sud América',
+        short_name: 'SUDA',
+      }),
+    ).toEqual({
+      country: 'AR',
+      location: 'Miramar',
+      name: 'Sud América',
+      short_name: 'SUDA',
+      id: expect.any(Number),
+    });
   });
 });
