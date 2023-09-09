10b3e9e0ce06774224ba0f7391de5c55ada4ff54
Lisandro Marchionni <marchionni.lisandro@gmail.com>
2023-09-09 11:57:59 -0300

    Testing Controller and Service (#3)
    * Use a Postgres container for testing database
    * Moved Envs to Global Scope

--- src/matchs/matchs.controller.spec.ts
+++ src/matchs/matchs.controller.spec.ts
@@ -1,45 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateTeamDto } from 'src/teams/dto/create-team-dto';
+import { Team } from 'src/teams/entities/team.entity';
+import { TeamsController } from 'src/teams/teams.controller';
+import { TeamsService } from 'src/teams/teams.service';
+import { Match } from './entities/matchs.entity';
 import { MatchsController } from './matchs.controller';
 import { MatchsService } from './matchs.service';
 
-describe('MatchsController', () => {
-  let controller: MatchsController;
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-  const mockMatchsService = {
-    add: jest.fn((dto) => {
-      return {
-        ...dto,
-        id: Date.now(),
-      };
-    }),
-  };
+describe('MatchsController', () => {
+  let matchsController: MatchsController;
+  let teamsController: TeamsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [MatchsController],
-      providers: [MatchsService],
-    })
-      .overrideProvider(MatchsService)
-      .useValue(mockMatchsService)
-      .compile();
+      controllers: [MatchsController, TeamsController],
+      providers: [MatchsService, TeamsService],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT),
+          username: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          entities: [Team, Match],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([Team, Match]),
+      ],
+    }).compile();
 
-    controller = module.get<MatchsController>(MatchsController);
+    matchsController = module.get<MatchsController>(MatchsController);
+    teamsController = module.get<TeamsController>(TeamsController);
   });
 
-  it('should create a team and return it with its id', () => {
+  it('should create a match between two teams and return it with its id', async () => {
+    const template: CreateTeamDto = {
+      country: 'AR',
+      location: 'Tandil',
+      name: 'Example Team',
+      short_name: 'TEAM',
+    };
+
+    const [team1, team2] = await Promise.all([
+      teamsController.addNewTeam({ ...template }),
+      teamsController.addNewTeam({ ...template }),
+    ]);
+
     expect(
-      controller.addNewMatch({
-        local: 1,
-        visitante: 2,
+      await matchsController.addNewMatch({
+        local: team1.id,
+        visitante: team2.id,
         sets_local: 2,
         sets_visitante: 3,
       }),
     ).toEqual({
-      id: expect.any(Number),
-      local: 1,
-      visitante: 2,
+      local: team1.id,
+      visitante: team2.id,
       sets_local: 2,
       sets_visitante: 3,
+      id: expect.any(Number),
     });
   });
 });
--- src/teams/teams.controller.spec.ts
+++ src/teams/teams.controller.spec.ts
@@ -1,44 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Team } from './entities/team.entity';
 import { TeamsController } from './teams.controller';
 import { TeamsService } from './teams.service';
+import { Match } from 'src/matchs/entities/matchs.entity';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 describe('TeamsController', () => {
   let controller: TeamsController;
-
-  const mockTeamsService = {
-    add: jest.fn((dto) => {
-      return {
-        id: Date.now(),
-        ...dto,
-      };
-    }),
-  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TeamsController],
       providers: [TeamsService],
-    })
-      .overrideProvider(TeamsService)
-      .useValue(mockTeamsService)
-      .compile();
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT),
+          username: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          entities: [Team, Match],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([Team]),
+      ],
+    }).compile();
 
     controller = module.get<TeamsController>(TeamsController);
   });
 
-  it('should create a team and return it with its id', () => {
+  it('should create a team and return it with its id', async () => {
     expect(
-      controller.addNewTeam({
+      await controller.addNewTeam({
         country: 'AR',
-        location: 'Miramar',
-        name: 'Sud América',
-        short_name: 'SUDA',
+        location: 'Tandil',
+        name: 'Example Team',
+        short_name: 'TEAM',
       }),
     ).toEqual({
       country: 'AR',
-      location: 'Miramar',
-      name: 'Sud América',
-      short_name: 'SUDA',
+      location: 'Tandil',
+      name: 'Example Team',
+      short_name: 'TEAM',
       id: expect.any(Number),
     });
   });
